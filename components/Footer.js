a3a05bf94142165522e3f267d64dc1532abefb4e
Soliperso <chah762002@yahoo.fr>
2021-12-09 12:58:03 -0800

    add styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Copyright &copy; {new Date().getFullYear()}</div>;
+  return <Footer>Copyright &copy; {new Date().getFullYear()}</Footer>;
 };
 
 export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
 import Link from 'next/link';
-import React from 'react';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-
+import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
-    <div>
+    <div className='styles.title'>
       <h1>Homepage</h1>
-      <p>
+      <p className='styles.text'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum esse pariatur eligendi
         odio dolorem rerum illo quia beatae dolores obcaecati? Illum ab voluptates odit, corporis
         amet voluptatum ratione dolorem, reprehenderit praesentium nihil cupiditate, magni soluta
         corrupti officia quos similique sequi quibusdam non repellat error maiores quo quis. Fuga,
         quisquam nisi.
       </p>
-      <p>
+      <p className='styles.text'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque magnam reprehenderit
         voluptates quis modi. Tempora a consequatur repellat debitis, alias doloribus? Dolorum rem
         consequuntur maxime, exercitationem saepe iste, placeat aperiam voluptas veniam, numquam
         veritatis commodi dolor accusantium iure porro at.
       </p>
       <Link href='/products'>
-        <a>See Products Listing</a>
+        <a className='styles.btn'>See Products Listing</a>
       </Link>
     </div>
   );
