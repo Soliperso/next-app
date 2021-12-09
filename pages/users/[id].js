dd1ab711d115b656d7144dea25a4101aaaa4597d
Soliperso <chah762002@yahoo.fr>
2021-12-09 14:44:26 -0800

    add a single item

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -2,22 +2,35 @@ export const getStaticPaths = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
   const data = await res.json();
 
-const paths = data.map(user => ({
+  const paths = data.map((user) => {
     return {
-        params: {id: user.id.toString()}
-    }
-}));
+      params: { id: user.id.toString() },
+    };
+  });
 
-return {
+  return {
     paths,
-    fallback: false
-};
+    fallback: false,
+  };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { user: data },
+  };
+};
+
+const Details = ({ user }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.website}</p>
+      <p>{user.address.city}</p>
     </div>
   );
 };
