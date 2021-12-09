8cbbbc129df6ac1315fd64f8405df8ac3dd7d4bc
Soliperso <chah762002@yahoo.fr>
2021-12-09 13:56:33 -0800

    add images && metadata

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
     <nav>
       <div className='logo'>
-        <h3>Products List</h3>
+        <Image src='/logo.jpeg' width={100} height='60' />
       </div>
       <Link href='/'>
         <a>Home</a>
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,33 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+
 export default function Home() {
   return (
-    <div className='styles.title'>
-      <h1>Homepage</h1>
-      <p className='styles.text'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum esse pariatur eligendi
-        odio dolorem rerum illo quia beatae dolores obcaecati? Illum ab voluptates odit, corporis
-        amet voluptatum ratione dolorem, reprehenderit praesentium nihil cupiditate, magni soluta
-        corrupti officia quos similique sequi quibusdam non repellat error maiores quo quis. Fuga,
-        quisquam nisi.
-      </p>
-      <p className='styles.text'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque magnam reprehenderit
-        voluptates quis modi. Tempora a consequatur repellat debitis, alias doloribus? Dolorum rem
-        consequuntur maxime, exercitationem saepe iste, placeat aperiam voluptas veniam, numquam
-        veritatis commodi dolor accusantium iure porro at.
-      </p>
-      <Link href='/products'>
-        <a className='styles.btn'>See Products Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Product List | Home</title>
+        <meta name='description' content='Product List' />
+      </Head>
+      <div>
+        <h1 className='styles.title'>Homepage</h1>
+        <p className='styles.text'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum esse pariatur eligendi
+          odio dolorem rerum illo quia beatae dolores obcaecati? Illum ab voluptates odit, corporis
+          amet voluptatum ratione dolorem, reprehenderit praesentium nihil cupiditate, magni soluta
+          corrupti officia quos similique sequi quibusdam non repellat error maiores quo quis. Fuga,
+          quisquam nisi.
+        </p>
+        <p className='styles.text'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque magnam reprehenderit
+          voluptates quis modi. Tempora a consequatur repellat debitis, alias doloribus? Dolorum rem
+          consequuntur maxime, exercitationem saepe iste, placeat aperiam voluptas veniam, numquam
+          veritatis commodi dolor accusantium iure porro at.
+        </p>
+        <Link href='/products'>
+          <a className='styles.btn'>See Products Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
